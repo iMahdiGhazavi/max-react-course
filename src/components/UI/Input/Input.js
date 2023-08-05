43cb63455b330d2d564c6f3e2dd8de1ac53e223a
mahdi <realmahdighazavi@gmail.com>
2023-08-05 11:40:40 +0330

    [feat]: adding the Input component and refactoring the login files

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from "./Input.module.css";
+
+const Input = ({ id, label, type, value, onChange, onBlur, isValid }) => {
+  return (
+    <div
+      className={`${classes.control} ${
+        isValid === false ? classes.invalid : ""
+      }`}
+    >
+      <label htmlFor={id}>{label}</label>
+      <input
+        type={type}
+        id={id}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+    </div>
+  );
+};
+
+export default Input;
