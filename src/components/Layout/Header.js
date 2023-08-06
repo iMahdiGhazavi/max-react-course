b4b5a40d2be2951e8c36fbaac8a779c1588677b4
mahdi <realmahdighazavi@gmail.com>
2023-08-06 10:12:02 +0330

    [feat]: adding the Cart Modal to the app

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,12 +3,12 @@ import classes from "./Header.module.css";
 import mealsImg from "../../assets/images/meals.jpg";
 import HeaderCartButton from "./HeaderCartButton";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImg} alt="A table full of delicious food!" />
