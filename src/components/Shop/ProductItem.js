cbfbe6db2a2189a9d47a58ec36c980b53d5865b5
mahdi <realmahdighazavi@gmail.com>
2023-08-13 22:38:27 +0330

    [feat]: adding more reactivity to the app and dispatching more redux actions

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -27,7 +27,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addToCartHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
