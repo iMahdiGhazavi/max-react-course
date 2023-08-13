a60adf271a2478eb32dd667e67006290ce954fea
mahdi <realmahdighazavi@gmail.com>
2023-08-13 22:33:39 +0330

    [feat]: rendering data onto the screen dynamically using Redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import uiSliceReducer from "./ui-slice";
+import CartSliceReducer from "./cart-slice";
 
 const store = configureStore({
   reducer: {
     ui: uiSliceReducer,
+    cart: CartSliceReducer,
   },
 });
 
