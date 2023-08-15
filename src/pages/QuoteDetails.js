dab3cc7a29c64124a375ea25391397704f06898f
mahdi <realmahdighazavi@gmail.com>
2023-08-15 21:50:12 +0330

    [init]: adding app's primary pages

---
+++ src/pages/QuoteDetails.js
@@ -0,0 +1,5 @@
+const QuoteDetails = () => {
+  return <div>QuoteDetails</div>;
+};
+
+export default QuoteDetails;
---
+++ src/pages/Quotes.js
@@ -0,0 +1,5 @@
+const Quotes = () => {
+  return <section></section>;
+};
+
+export default Quotes;
