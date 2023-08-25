9191c306987b47823fbb7b9b3f4047895c2d9935
mahdi <realmahdighazavi@gmail.com>
2023-08-25 10:16:16 +0330

    [feat]: adding user authentication persistance to the app

--- src/store/AuthContextProvider.js
+++ src/store/AuthContextProvider.js
@@ -2,16 +2,19 @@ import React, { useState, useEffect } from "react";
 import AuthContext from "./auth-context";
 
 const AuthContextProvider = (props) => {
+  const initialToken = localStorage.getItem("token");
   const [token, setToken] = useState(null);
 
   const userIsLoggedIn = !!token;
 
   const loginHandler = (token) => {
     setToken(token);
+    localStorage.setItem("token", token);
   };
 
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem("token");
   };
 
   useEffect(() => {
