8170180991f892244c19bfb218fb2ab7395d65e6
mahdi <realmahdighazavi@gmail.com>
2023-08-25 11:05:56 +0330

    [feat]: handling user auto logout process

--- src/store/AuthContextProvider.js
+++ src/store/AuthContextProvider.js
@@ -1,21 +1,35 @@
 import React, { useState, useEffect } from "react";
 import AuthContext from "./auth-context";
 
+const calculateRemainingTime = (expirationTime) => {
+  const currentTime = new Date().getTime();
+  const adjExpirationTime = new Date(expirationTime).getTime();
+
+  const remainingDuration = adjExpirationTime - currentTime;
+
+  return remainingDuration;
+};
+
 const AuthContextProvider = (props) => {
   const initialToken = localStorage.getItem("token");
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token;
-
-  const loginHandler = (token) => {
-    setToken(token);
-    localStorage.setItem("token", token);
-  };
 
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem("token");
   };
+
+  const loginHandler = (token, expirationTime) => {
+    setToken(token);
+    localStorage.setItem("token", token);
+
+    const remainingTime = calculateRemainingTime(expirationTime);
+
+    setTimeout(logoutHandler, remainingTime);
+  };
+
 
   useEffect(() => {
     const storedToken = localStorage.getItem("token");
