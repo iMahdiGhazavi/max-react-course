d99e384095c97231ffefe17c957916784842db96
mahdi <realmahdighazavi@gmail.com>
2023-08-28 08:16:17 +0330

    [feat]: replacing redux with the context api in products components

---
+++ src/context/ProductsProvider.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+import ProductsContext from "./products-context";
+
+const INITIAL_PRODUCTS = [
+  {
+    id: "p1",
+    title: "Red Scarf",
+    description: "A pretty red scarf.",
+    isFavorite: false,
+  },
+  {
+    id: "p2",
+    title: "Blue T-Shirt",
+    description: "A pretty blue t-shirt.",
+    isFavorite: false,
+  },
+  {
+    id: "p3",
+    title: "Green Trousers",
+    description: "A pair of lightly green trousers.",
+    isFavorite: false,
+  },
+  {
+    id: "p4",
+    title: "Orange Hat",
+    description: "Street style! An orange hat.",
+    isFavorite: false,
+  },
+];
+
+const ProductsProvider = (props) => {
+  const [productsList, setProductsList] = useState(INITIAL_PRODUCTS);
+  return (
+    <ProductsContext.Provider value={{ products: productsList }}>
+      {props.children}
+    </ProductsContext.Provider>
+  );
+};
+
+export default ProductsProvider;
