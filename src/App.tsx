d10a57dd36d3dc99df7fedbb994c8a12a280cc86
mahdi <realmahdighazavi@gmail.com>
2023-08-30 12:22:25 +0330

    [feat]: adding NewTodo logic and some stylings

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
+
 import Todos from "./components/Todos";
+import NewTodo from "./components/NewTodo";
 import Todo from "./models/todo";
 
 function App() {
-  const todos = [
-    new Todo("Learn React"),
-    new Todo("Learn TypeScript"),
-    new Todo("Learn Next.js"),
-  ];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
