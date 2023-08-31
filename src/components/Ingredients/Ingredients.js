5bbf318602a780f1792ae96b4cb30bdc2cfac95b
mahdi <realmahdighazavi@gmail.com>
2023-08-31 11:39:27 +0330

    [feat]: adding ingredients add and remove logics

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
 import Search from "./Search";
 
+const INITIAL_INGREDIENTS = [
+  { id: "1", title: "Apples", amount: 5 },
+  { id: "2", title: "Tomatoes", amount: 3 },
+  { id: "3", title: "Potatoes", amount: 6 },
+];
+
 function Ingredients() {
+  const [userIngredients, setUserIngredients] = useState(INITIAL_INGREDIENTS);
+
+  const addIngredientHandler = (ingredient) => {
+    setUserIngredients((prevIngredients) => [
+      ...prevIngredients,
+      { id: Math.random().toString(), ...ingredient },
+    ]);
+  };
+
+  const removeIngredientHandler = (ingredientId) => {
+    setUserIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+    );
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
