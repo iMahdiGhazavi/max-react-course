fd6da80a3afb99f9abfd098f3b0cb987e0275c0c
mahdi <realmahdighazavi@gmail.com>
2023-09-03 22:23:26 +0330

    [feat]: adding the events page to the app

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,29 +1,22 @@
-import { Link } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
-const DUMMY_EVENTS = [
-  {
-    id: "e1",
-    title: "Some Event",
-  },
-  {
-    id: "e2",
-    title: "Another Event",
-  },
-];
+import EventsList from "../components/EventsList";
 
-const EventsPage = () => {
-  return (
-    <>
-      <h1>Events Page</h1>
-      <ul>
-        {DUMMY_EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={event.id}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-};
+function EventsPage() {
+  const events = useLoaderData();
+
+  return <EventsList events={events} />;
+}
 
 export default EventsPage;
+
+export const loader = async () => {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    //...
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+};
