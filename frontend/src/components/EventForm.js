dc194534b71053afa370ae41096fba929ef37c18
mahdi <realmahdighazavi@gmail.com>
2023-09-04 10:52:50 +0330

    [feat]: adding some feedback for form submission

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,8 +1,12 @@
-import { Form } from "react-router-dom";
+import { Form, useNavigation } from "react-router-dom";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === "submitting";
+
   function cancelHandler() {}
 
   return (
@@ -48,10 +52,12 @@ function EventForm({ method, event }) {
         />
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? "Submitting..." : "Send"}
+        </button>
       </div>
     </Form>
   );
