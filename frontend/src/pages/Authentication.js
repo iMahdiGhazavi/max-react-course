231828bab23bd3f158cc289916df260c09540d4f
mahdi <realmahdighazavi@gmail.com>
2023-09-05 11:03:08 +0330

    [feat]: handling received token from back-end

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -34,5 +34,9 @@ export const action = async ({ request }) => {
     throw json({ message: "Could not authenticate user." }, { status: 500 });
   }
 
+  const responseData = await response.json();
+  const token = responseData.token;
+
+  localStorage.setItem("token", token);
   return redirect("/");
 };
